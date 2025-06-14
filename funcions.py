c156d6eab0dc904de8ef4a97ec14189b6f2564a1
Marc111-code <solerm.marc@gmail.com>
2025-06-14 14:15:22 +0200

    Update funcions.py

--- funcions.py
+++ funcions.py
@@ -1,21 +1,24 @@
+score = 0 
 def problema1():
+    global score
     print("La meitat del doble de 12, més 8, dividit per 4.")
     resposta = input("Resposta: ")
     if resposta == "5":
-        print("Molt bè, ho has aconseguit!")
-        print("Passes a la següent sala")
+        print("Molt bé, ho has aconseguit!")
+        score += 3
     else:
-        while(resposta != "5"):
+        while resposta != "5":
             print("Torna a intentar!")
             print("La meitat del doble de 12, més 8, dividit per 4.")
             resposta = input("Resposta: ")
         print("Molt Bé")
-        print("Passes a la següent sala")
-
+        score += 1
+    print("Passes a la següent sala")
 
 import random
 
 def problema2():
+    global score
     print("En aquesta segona sala hauràs de descobrir quina és la paraula amagada")
     pistes = [
         "Comença amb la lletra 'o'.",
@@ -23,46 +26,59 @@ def problema2():
         "Acaba amb la lletra 'a'.",
         "Es relaciona amb la llum i la foscor.",    
     ]
-    pista = random.choice(pistes)
     print("Sóc una paraula de 5 lletres.")
     print("Si treus la primera lletra, encara so com abans.")
     print("Si treus la segona lletra, també.")
     print("Fins i tot si només queda una lletra, la paraula segueix sent la mateixa.")
     print("Quina paraula sóc?")
+    
     resposta = input("Resposta: ")
-    if resposta in ["ombra","Ombra","OMBRA"]:
+    intents = 1
+    while resposta.lower() != "ombra":
+        if len(pistes) > 0:
+            pista = random.choice(pistes)
+            print("Pista:", pista)
+            pistes.remove(pista)
+        else:
+            print("No hi ha més pistes disponibles")
+        resposta = input("Resposta: ")
+        intents += 1
+
+    if intents == 1:
         print("Molt bé, a la primera!")
-        print("Passes a la següent sala")
+        score += 3
     else:
-        while resposta not in ["ombra","Ombra","OMBRA"]:
-            if len(pistes) > 0:
-                pista = random.choice(pistes)
-                print("Pista:", pista)
-                pistes.remove(pista)
-                resposta = input("Resposta: ")
-            else:
-                print("No hi ha més pistes disponibles")
-                resposta = input("Resposta: ")
         print("Molt bé, passes a la següent sala!")
+        score += 1
 
 def problema3():
+    global score
     print("Heu entrat a la sala d'història")
-    print("Sabeu quin any es ve descobrir Amèrica, quin?")
-    resposta = input("Resposta: ")
-    if resposta == "1492":
+    
+    resposta = input("Sabeu quin any es ve descobrir Amèrica, quin? ")
+    intents = 1
+    while resposta != "1492":
+        print("Torna a intentar!")
+        resposta = input("Resposta: ")
+        intents += 1
+    if intents == 1:
         print("Tu no seràs un expert en història, no?")
+        score += 3
     else:
-        while(resposta != "1492"):
-            print("Torna a intentar!")
-            resposta = input("Resposta: ")
         print("Molt Bé")
-    print("Quin any va ser la revolució francesa?")
-    respostaf = input("Resposta: ")
-    if respostaf == "1789":
+        score += 1
+
+    respostaf = input("Quin any va ser la revolució francesa? ")
+    intents = 1
+    while respostaf != "1789":
+        print("Torna a intentar!")
+        respostaf = input("Resposta: ")
+        intents += 1
+    if intents == 1:
         print("Fàcil?")
-        print("Ara passaràs a la següent sala")
+        score += 3
     else:
-        while(respostaf != "1789"):
-            print("Torna a intentar!")
-            respostaf = input("Resposta: ")
         print("Molt Bé")
+        score += 1
+
+    print("Ara passaràs a la següent sala")
