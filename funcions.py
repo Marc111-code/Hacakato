c214b8acd7fbeb096dd6c13af1e3dbcd368406b5
Marc111-code <solerm.marc@gmail.com>
2025-06-14 12:08:04 +0200

    Update funcions.py

--- funcions.py
+++ funcions.py
@@ -1,1 +1,16 @@
+def problema1():
+    print("La meitat del doble de 12, més 8, dividit per 4.")
+    resposta = input("Resposta: ")
+    if resposta == "5":
+        print("Molt bè, ho has aconseguit!")
+        print("Passes a la següent sala")
+    else:
+        while(resposta != "5"):
+            print("Torna a intentar!")
+            print("La meitat del doble de 12, més 8, dividit per 4.")
+            resposta = input("Resposta: ")
+        print("Molt Bé")
+        print("Passes a la següent sala")
+
+
 
